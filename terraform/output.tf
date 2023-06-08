112b820b0b52e1bdefcb6cbf008a380422d3a32e
kolosovpetro <kolosovp94@gmail.com>
2023-06-08 12:49:25 +0200

    output update

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,7 @@
-output "vm_public_ip_address" {
+output "vm_public_key_auth_ip_address" {
   value = module.ubuntu-vm-public-key-auth.vm_public_ip_address
+}
+
+output "vm_password_auth_ip_address" {
+  value = module.ubuntu-vm-password-auth.vm_public_ip_address
 }
